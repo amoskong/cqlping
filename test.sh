45dd7a4199f7ba75b18d787735ffcf242766a698
Amos Kong <amos@scylladb.com>
2017-12-30 06:06:33 +0800

    make verify-query only optional in debug mode
    One kind of queries(INSERT/SELECT/UPDATE/DELETE/ALTER) is enough
    for ping, the verify-query can only be used in debug mode. We don't
    need to verify the first query result in regural case.
    
    Signed-off-by: Amos Kong <amos@scylladb.com>

--- test.sh
+++ test.sh
@@ -7,23 +7,29 @@ echo "2. You should run cassandra-stress write ... to create keyspace1.standard1
 
 echo "3. Test with keyspace1.standard1 created by C*-stress"
 
-echo "3.1.1 test with random string"
+echo "3.1.1 test with one random string (without verify)"
 ./cqlping scylla-server \
   --request-query "INSERT INTO keyspace1.standard1 (key,\"C0\") VALUES (textAsBlob('1'), textAsBlob('%s'))" \
-  --verify-query "select * from keyspace1.standard1 where key=textAsBlob('1')" -c 2 -s 10 --debug
+  -c 2 -s 10 --debug
 
-echo "3.1.2 test with fixed string"
+echo "3.1.2 test with one random string (with verify)"
 ./cqlping scylla-server \
-  --request-query "INSERT INTO keyspace1.standard1 (key,\"C0\") VALUES (textAsBlob('1'), textAsBlob('hello'))" \
-  --verify-query "select * from keyspace1.standard1 where key=textAsBlob('1')" -c 2 --debug
+  --request-query "INSERT INTO keyspace1.standard1 (key,\"C0\") VALUES (textAsBlob('1'), textAsBlob('%s'))" \
+  --verify-query "select * from keyspace1.standard1 where key=textAsBlob('1')" \
+  -c 2 -s 10 --debug
 
+echo "3.1.3 test with multiple fixed string"
+./cqlping scylla-server \
+  --request-query "INSERT INTO keyspace1.standard1 (key,\"C0\",\"C1\") VALUES (textAsBlob('1'), textAsBlob('hello'),                textAsBlob('world'))" \
+  -c 2 --debug
 
 echo "3.2.1 test with text Column"
 cqlsh scylla-server -e "CREATE TABLE IF NOT EXISTS cqlping.cf_txt (key1 bigint, val text, PRIMARY KEY(key1));"
 
 ./cqlping scylla-server \
   --request-query "INSERT INTO cqlping.cf_txt (key1, val) VALUES (1, '%s')" \
-  --verify-query "select * from cqlping.cf_txt where key1=1" -c 2 -s 2 --debug
+  --verify-query "select * from cqlping.cf_txt where key1=1" \
+  -c 2 -s 2 --debug
 
 
 echo "3.2.2 test with blob Column"
@@ -31,7 +37,8 @@ cqlsh scylla-server -e "CREATE TABLE IF NOT EXISTS cqlping.cf_blob (key1 bigint,
 
 ./cqlping scylla-server \
   --request-query "INSERT INTO cqlping.cf_blob (key1, val) VALUES (1, textAsBlob('%s'))" \
-  --verify-query "select * from cqlping.cf_blob where key1=1" -c 2 -s 2 --debug
+  --verify-query "select * from cqlping.cf_blob where key1=1" \
+  -c 2 -s 2 --debug
 
 
 echo "3.2.3 test with varchar Column"
@@ -39,4 +46,5 @@ cqlsh scylla-server -e "CREATE TABLE IF NOT EXISTS cqlping.cf_varchar (key1 bigi
 
 ./cqlping scylla-server \
   --request-query "INSERT INTO cqlping.cf_txt (key1, val) VALUES (1, '%s')" \
-  --verify-query "select * from cqlping.cf_txt where key1=1" -c 2 -s 2 --debug
+  --verify-query "select * from cqlping.cf_txt where key1=1" \
+  -c 2 -s 2 --debug
